5594500a4d0d58a5fbbcf600951ca1607edeef4e
SUKHSAGAR86 <sagarjangde86@gmail.com>
2026-01-01 17:23:06 +0530

    add page for client module

---
+++ app/client/page.js
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import Dashboard from "@/components/client/dashboard";
+import NoticeBoard from "@/components/Newspaper/noticeboard";
+import { Box, Divider } from "@mui/material";
+
+function ReleaseOrderListing() {
+  // Mock data
+  const mockData = [
+    { id: 1, title: "Dainik Bhaskar" },
+    { id: 2, title: "Patrika" },
+    { id: 3, title: "Navbharat Times" },
+    { id: 4, title: "Haribhoomi" },
+    { id: 5, title: "The Hitavada" },
+  ];
+
+  return (
+    <Box>
+      <Box sx={{ width: "100%" }}>
+        <Dashboard />
+      </Box>
+      <Box>
+        <Divider sx={{ mb: 3 }} />
+        <NoticeBoard />
+      </Box>
+    </Box>
+  );
+}
+
+export default ReleaseOrderListing;
