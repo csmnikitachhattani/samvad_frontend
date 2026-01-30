db85ab445089a6e415d2df00c9adb79ba04b53e4
csmnikitachhattani <nikita.chhattani@csm.tech>
2026-01-30 16:41:54 +0530

    add vehicle master in admin

--- app/admin/vehicle/page.js
+++ app/admin/vehicle/page.js
@@ -1,17 +1,14 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import outdoorService from "@/services/outdoorServices";
-import CreateEditModal from '@/components/outdoor/vehicle/createEditModal'
-import VehicleTables from '@/components/outdoor/vehicle/VehicleList'
+import CreateEditModal from '@/components/admin/vehicle/createEditModal'
+import VehicleTables from '@/components/admin/vehicle/VehicleList'
 import {
   Box,
 } from "@mui/material";
 
 const DisplayBoardTable = () => {
   const [data, setData] = useState([]);
-
- 
-
   return (
     <Box sx={{ p: 4, backgroundColor: "#fafafa", minHeight: "100vh" }}>
       <VehicleTables />
