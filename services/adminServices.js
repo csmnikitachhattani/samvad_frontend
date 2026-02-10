a52fe6cb459c51756f95299cef55fd0711bed41e
nikita.chhattani@csm.tech <nikita.chhattani@csm.tech>
2026-02-10 15:19:11 +0530

    changes foe allocation form

--- services/adminServices.js
+++ services/adminServices.js
@@ -60,6 +60,17 @@ const adminService = {
       throw err; // interceptor will format it
     }
   },
+  getVendorList: async (id) => {
+    console.log("get counter detail")
+    try {
+      const res = await axiosClient.get(`http://103.79.34.50:8083/api/ManageMaster/getagencybyid${id}`);
+      return res.data;
+    } catch (err) {
+      console.log("answer")
+      throw err; // interceptor will format it
+    }
+  },
+  
 
 
 };
