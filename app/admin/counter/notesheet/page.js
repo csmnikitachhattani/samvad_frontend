49ba554b65dfd67a62cf1fdf771e1b1a9fd68ac5
nikita.chhattani@csm.tech <nikita.chhattani@csm.tech>
2026-02-23 22:41:54 +0530

    fix the Issue fir serach params for suspense

--- app/admin/counter/notesheet/page.js
+++ app/admin/counter/notesheet/page.js
@@ -1,19 +1,11 @@
-"use client";
+import { Suspense } from "react";
+import NotesheetClient from "@/components/admin/counter/notesheetDetail"
 
-import { useSearchParams } from "next/navigation";
-import NotesheetForm from "@/components/admin/counter/notesheetDetail"
-export default function CounterPage() {
-  const searchParams = useSearchParams();
-
-  const id = searchParams.get("id");
-  const finYear = searchParams.get("fin_year");
-
+export default function Page() {
   return (
-    <div>
-      <h1>Counter Page</h1>
-      {/* <p>ID: {id}</p>
-      <p>Financial Year: {finYear}</p> */}
-      <NotesheetForm />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+        <h1></h1>
+      <NotesheetClient />
+    </Suspense>
   );
 }
--- app/admin/workorder/create/page.js
+++ app/admin/workorder/create/page.js
@@ -1,14 +1,17 @@
 "use client";
 import DetailSummary from "@/components/admin/workorder/detailSummary"
+import { Suspense } from "react";
 export default function CounterPage() {
 
 
   return (
+    <Suspense fallback={<div>Loading...</div>}>
     <div>
       <h1>Work Order Pending Page</h1>
      <div sx={{margin: '10'}}>
      <DetailSummary />
     </div>
     </div>
+    </Suspense>
   );
 }
