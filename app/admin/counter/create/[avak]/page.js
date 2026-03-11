88164dcd9add510aeed2ad218f6f6cfcae573493
nikita.chhattani@csm.tech <nikita.chhattani@csm.tech>
2026-03-11 21:49:14 +0530

    changes for counter

--- app/admin/counter/create/[avak]/page.js
+++ app/admin/counter/create/[avak]/page.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import axiosClient from "@/lib/axiosClient";
 import adminServices from "@/services/adminServices";
 import { useParams } from "next/navigation";
+import commonServices from "@/services/commonServices";
 import clientServices from "@/services/clientServices";
 import {
   Box,
@@ -59,6 +60,12 @@ export default function JobForm() {
   const router = useRouter();
   const dispatch = useDispatch();
   const [services, setServices] = useState([]);
+  const [departments, setDepartments] = useState([])
+  const [districts, setDistricts] = useState([])
+  const [levels, setLevels] = useState([])
+  const [offices, setOffices] = useState([])
+  const [sections, setSections] = useState([])
+  const [officers, setOfficers] = useState([])
   const durationOptions = [
     { value: "1week", label: "1 Week" },
     { value: "2week", label: "2 Weeks" },
@@ -89,21 +96,27 @@ export default function JobForm() {
     office_code: "",
     district_code: "",
     remarks: "",
+    baseDept: '',
+    district: '',
+    officeLevel: '',
+    office: '',
+    section: '',
     ip_address: "",
+    client: "",
     entry_user_name: "nikita",
     files: null,
   });
-  useEffect(() => {
-    async function fetchServices() {
-      try {
-        const response = await adminServices.getServices();
-        setServices(response?.result || []);
-      } catch (error) {
-        console.error("Failed to fetch services", error);
-      }
-    }
-    fetchServices();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchServices() {
+  //     try {
+  //       const response = await adminServices.getServices();
+  //       setServices(response?.result || []);
+  //     } catch (error) {
+  //       console.error("Failed to fetch services", error);
+  //     }
+  //   }
+  //   fetchServices();
+  // }, []);
   const { avak } = useParams();
   useEffect(() => {
     async function fetchAvakDetails() {
@@ -126,6 +139,8 @@ export default function JobForm() {
       }
     }
     fetchAvakDetails();
+    fetchDepartment();
+    fetchDistricts();
   }, []);
   const handleChange = (e) => {
     const { name, value, files } = e.target;
@@ -143,6 +158,119 @@ export default function JobForm() {
   useEffect(() => {
     getPublicIP()
   })
+
+  async function fetchDepartment() {
+    try {
+      const res = await clientServices.getalldepartment();
+      setDepartments(res.result)
+    } catch (error) {
+      console.error("Failed to fetch work orders", error);
+    } finally {
+      //setLoading(false);
+    }
+  }
+  async function fetchOfficeLevel(deptCode) {
+    try {
+      const res = await clientServices.getOfficeLevels({deptCode});
+      setLevels(res.result)
+    } catch (error) {
+      console.error("Failed to fetch work orders", error);
+    } finally {
+      //setLoading(false);
+    }
+  }
+  async function fetchDistricts() {
+    try {
+      const res = await commonServices.getDistrict();
+      setDistricts(res.data.result);
+    } catch (error) {
+      console.error("Failed to fetch work orders", error);
+    } finally {
+      //setLoading(false);
+    }
+  }
+  async function fetchOffice(deptCode, distCode) {
+    try {
+      const res = await clientServices.getOfficeNames({deptCode, distCode});
+      console.log(res)
+      setOffices(res.result);
+    } catch (error) {
+      console.error("Failed to fetch work orders", error);
+    } finally {
+      //setLoading(false);
+    }
+  }
+  async function fetchSections(deptCode, distCode) {
+    try {
+      const res = await clientServices.getClientSection({deptCode, distCode});
+      console.log(res)
+      setSections(res.result);
+    } catch (error) {
+      console.error("Failed to fetch work orders", error);
+    } finally {
+      //setLoading(false);
+    }
+  }
+  async function fetchOfficers(deptCode, distCode) {
+    try {
+      const res = await clientServices.getOfficers({deptCode, distCode});
+      console.log(res)
+      setOfficers(res.result);
+    } catch (error) {
+      console.error("Failed to fetch work orders", error);
+    } finally {
+      //setLoading(false);
+    }
+  }
+  async function fetchCaption() {
+    try {
+      const res = await clientServices.getAdvtCaption();
+      setCaptions(res.data.result);
+    } catch (error) {
+      console.error("Failed to fetch work orders", error);
+    } finally {
+      //setLoading(false);
+    }
+  }
+
+  async function fetchClient(client) {
+    try {
+      const res = await clientServices.getClientData(client);
+      console.log("console", res.data)
+      setData((prev) => ({
+        ...prev,
+        baseDept: res.data.data.base_dept_code,
+        district: res.data.data.district_code,
+        officeLevel: res.data.data.OfficeLevel,
+        office: res.data.data.Office_code,
+        section: res.data.data.section_code,
+        officer: res.data.data.employee_code,
+      }));
+    fetchDistricts();
+    } catch (error) {
+      console.error("Failed to fetch work orders", error);
+    } finally {
+      //setLoading(false);
+    }
+  }
+  useEffect(() => {
+    if(data.client !== ""){
+      fetchClient(data.client)
+    }
+  },[data.client])
+  useEffect(() => {
+    if(data.baseDept&&data.district){
+    fetchOfficeLevel(data.baseDept)
+    fetchOffice(data.baseDept, data.district);
+    fetchSections(data.baseDept, data.district);
+    fetchOfficers(data.baseDept, data.district);
+    }
+  },[data.baseDept, data.district])
+  useEffect(() => {
+    if(data.baseDept){
+    fetchOfficeLevel(data.baseDept)
+    }
+  },[data.baseDept,])
 
   const handleSubmit = async (e) => { 
     e.preventDefault();
@@ -523,35 +651,51 @@ export default function JobForm() {
            {/* ── Section 3: Location & Office ── */}
            <SectionCard  title="Office & Location" subtitle="Departmental and geographic assignment" accent="#10b981">
             <Grid container spacing={2.5}>
-              <Grid item size={{ xs: 12, }}>
+              <Grid item size={{ xs: 12, md: 4 }}>
                 <TextField
                   fullWidth
+                  label="Client"
+                  name="client"
+                  value={data.client}
+                  onChange={handleChange}
+                  sx={field}
+                />
+              </Grid>
+              <Grid item size={{xs:12}}>
+                <TextField
+
+                  fullWidth
+                  select
                   label="Base Department"
                   name="baseDept"
                   value={data.baseDept}
                   onChange={handleChange}
                   sx={field}
-                />
+                >
+                  {departments.map((dept) => (
+                    <MenuItem key={dept.deptid} value={dept.deptid}>
+                      {dept.deptname}
+                    </MenuItem>
+                  ))}
+                </TextField>
               </Grid>
 
               <Grid item size={{ xs: 12, }}>
                 <TextField
+                 select
                   fullWidth
                   label="District"
                   name="district"
                   value={data.district}
                   onChange={handleChange}
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <svg width="15" height="15" viewBox="0 0 24 24" fill="none">
-                          <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7z" stroke="#9ca3af" strokeWidth="1.8" strokeLinecap="round" />
-                        </svg>
-                      </InputAdornment>
-                    ),
-                  }}
                   sx={field}
-                />
+                >
+                   {districts.map((district) => (
+                    <MenuItem key={district.dstrictid} value={district.dstrictid}>
+                      {district.districtname}
+                    </MenuItem>
+                  ))}
+                  </TextField>
               </Grid>
 
               <Grid item size={{ xs: 12, md: 4 }}>
@@ -564,22 +708,31 @@ export default function JobForm() {
                   onChange={handleChange}
                   sx={field}
                 >
-                  <MenuItem value="state">State Level</MenuItem>
-                  <MenuItem value="district">District Level</MenuItem>
-                  <MenuItem value="block">Block Level</MenuItem>
-                  <MenuItem value="panchayat">Panchayat Level</MenuItem>
+                  
+                  {levels.map((level) => (
+                    <MenuItem key={level.officeLevelCode} value={level.officeLevelCode}>
+                      {level.officeLevelName}
+                    </MenuItem>
+                  ))}
                 </TextField>
               </Grid>
 
               <Grid item size={{ xs: 12, md: 4 }}>
                 <TextField
+                  select
                   fullWidth
                   label="Office"
                   name="office"
                   value={data.office}
                   onChange={handleChange}
                   sx={field}
-                />
+                >
+                    {offices.map((office) => (
+                    <MenuItem key={office.newOfficeCode} value={office.newOfficeCode}>
+                      {office.officeName}
+                    </MenuItem>
+                  ))}
+                </TextField>
               </Grid>
 
               <Grid item size={{ xs: 12, md: 4 }}>
@@ -590,18 +743,31 @@ export default function JobForm() {
                   value={data.section}
                   onChange={handleChange}
                   sx={field}
-                />
+                > {sections.map((section) => (
+                  <MenuItem key={section} >
+                    {section}
+                  </MenuItem>
+                ))}
+
+                  </TextField>
               </Grid>
 
               <Grid item size={{ xs: 12, md: 4 }}>
                 <TextField
+                  select
                   fullWidth
                   label="Officer"
                   name="officer"
                   value={data.officer}
                   onChange={handleChange}
                   sx={field}
-                />
+                >
+                    {officers.map((officer) => (
+                    <MenuItem key={officer.employeeId} value={officer.employeeId}>
+                      {officer.employeeName}
+                    </MenuItem>
+                  ))}
+                  </TextField>
               </Grid>
             </Grid>
           </SectionCard>
