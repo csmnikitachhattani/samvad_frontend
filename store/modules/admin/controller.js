3aebf92bb1ec2fb507bc45dca8df3e1c6faf18d5
nikita.chhattani@csm.tech <nikita.chhattani@csm.tech>
2026-03-13 14:03:35 +0530

    changes for components

---
+++ store/modules/admin/controller.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+
+const adminControllerSlice = createSlice({
+  name: "vehicle",
+  initialState: {
+    value: 0,
+    ModalShow: false,
+    statusModalShow: false,
+    
+  },
+  reducers: {
+    toggleModal: (state, action) =>{
+      console.log("calling store function ")
+      state.ModalShow= action?.payload?.show 
+      console.log(state.ModalShow)
+
+    },
+    toggleStatusModal: (state, action) =>{
+      state.statusModalShow = !state.statusModalShow
+      state.roDetails= action?.payload || {}
+      console.log(state.roDetails)
+    },
+    
+  },
+});
+
+
+
+
+                                        
+
+
+
+
+
+export const { toggleModal, } = adminControllerSlice.actions;
+export default adminControllerSlice.reducer;
