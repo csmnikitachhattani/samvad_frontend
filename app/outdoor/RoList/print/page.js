13f4f719985acb3d99f4575961939e6ad0bdf76d
nikita.chhattani@csm.tech <nikita.chhattani@csm.tech>
2026-04-06 10:19:18 +0530

    changes for RO print

---
+++ app/outdoor/RoList/print/page.js
@@ -0,0 +1,10 @@
+import { Suspense } from "react";
+import NotesheetPrint from "@/components/admin/allocation/NotesheetPrint"
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NotesheetPrint />
+    </Suspense>
+  );
+}
