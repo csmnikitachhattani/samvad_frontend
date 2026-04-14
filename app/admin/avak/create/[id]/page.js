7c4cc4d12ebd0e20cd5228a6b4dbc7418abd5b32
nikita.chhattani@csm.tech <nikita.chhattani@csm.tech>
2026-04-14 13:15:26 +0530

    Avak Fixed

--- app/admin/avak/create/[id]/page.js
+++ app/admin/avak/create/[id]/page.js
@@ -272,35 +272,6 @@ export default function ClientAttachmentForm() {
     isIndividual: false,
   });
 
-  // ── Local Storage ──
-  const [financialYear, setFinancialYear] = useState("");
-  const [userId,        setUserId]        = useState("");
-  const [user_name,     setUserName]      = useState("");
-  const [userTypeCd,   setUserTypeCd]      = useState("");
-
-  useEffect(() => {
-    if (typeof window === "undefined") return;
-    // localStorage.setItem("financialYear", localStorage.getItem("financialYear") );
-    // localStorage.setItem("user_id",        localStorage.getItem("userid")       );
-    // localStorage.setItem("refCategoryId", localStorage.getItem("refCategoryId") );
-    // localStorage.setItem("user_name",     localStorage.getItem("user_name") );
-    console.log("running")
-    setFinancialYear(localStorage.getItem("financialYear"));
-    setUserId(localStorage.getItem("userid"));
-    setUserName(localStorage.getItem("user_name"));
-    setUserTypeCd(localStorage.getItem("usertypecode"));
-    const getIP = async () => {
-      try {
-        const res  = await fetch("https://api.ipify.org?format=json");
-        const data = await res.json();
-        setFormData(p => ({ ...p, ip_address: data.ip }));
-        return data.ip
-      } catch { console.error("IP fetch failed"); }
-    };
-    getIP();
-  }, []);
-
-
   // ── handleChange clears the field's error on edit ───────────────────────────
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
@@ -563,15 +534,15 @@ export default function ClientAttachmentForm() {
         fixed_date: formData.fixedDate ? "Y" : "N",
         tender_amt: Number(formData.tender_amt) || 0,
         letter_no: formData.letter_no,
-        letter_date: formData.letter_date
-          ? new Date(formData.letter_date).toISOString().slice(0, 10)
+        letter_date: formData.letterDate
+          ? new Date(formData.letterDate).toISOString().slice(0, 10)
           : null,
         caption_cd: formData.captionCd || "02",
         total_pages: String(formData.files),
         receiving_mode_code: formData.modeOfReceiving,
         letter_type_code: formData.letterType,
         remarks: formData.remark,
-        financial_year: formData.financialYear || financialYear,
+        financial_year: formData.financialYear || "2024-2025",
         client_cd: String(formData.officer),
         base_dept_code: formData.baseDept,
         office_code: formData.office,
@@ -580,23 +551,22 @@ export default function ClientAttachmentForm() {
         section_code: formData.section,
         client_prarup_code: formData.clientPrarupCode || "2",
         client_name: formData.client_name,
-        //...(formData.isIndividual && {
-        client_address: formData.clientAddress || formData.district,
-        client_city: formData.clientCity || formData.office,
-        //}),
+        ...(formData.isIndividual && {
+        client_address: formData.clientAddress || 'NA',
+        client_city: formData.clientCity || "NA",
+        }),
         schedule_date: formData.schedule_date
           ? new Date(formData.schedule_date).toISOString().slice(0, 10)
           : null,
         ref_id: id,
         create_update_flag_name: "Insert",
-        entry_by_user_type_cd: userTypeCd,
-        entry_by_user_id: userId,
-        ip_address: getIP() || "103.79.34.50",
+        entry_by_user_type_cd: "01",
+        entry_by_user_id: "00100",
         entry_by_section_cd: formData.section || "",
         client_exist: "Y",
         entry_date: new Date().toISOString().slice(0, 10),
         entry_time: new Date().toTimeString().split(" ")[0],
-     
+        ip_address: "103.79.34.50",
       };
 
       const response = await axiosClient.post("/Client/create-update-avak", payload, {
